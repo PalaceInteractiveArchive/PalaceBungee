ece32b7cf9f7ddd56013718492dddb4c6e2eb165
Marc DiFilippo <marc@marcdif.com>
2021-02-10 20:32:16 -0500

    Fix announcement formatting

--- src/main/java/network/palace/bungee/utils/BroadcastUtil.java
+++ src/main/java/network/palace/bungee/utils/BroadcastUtil.java
@@ -1,8 +1,6 @@
 package network.palace.bungee.utils;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
 import network.palace.bungee.PalaceBungee;
 import network.palace.bungee.handlers.Player;
 
@@ -22,13 +20,10 @@ public class BroadcastUtil {
                 if (announcements.isEmpty()) return;
                 if (i >= announcements.size()) i = 0;
 
-                BaseComponent[] components = new ComponentBuilder("[").color(ChatColor.WHITE)
-                        .append("✦").color(ChatColor.BLUE).append("] ").color(ChatColor.WHITE)
-                        .appendLegacy(announcements.get(i++))
-                        .create();
+                String message = ChatColor.WHITE + "[" + ChatColor.BLUE + "✦" + ChatColor.WHITE + "] " + announcements.get(i++);
 
                 for (Player tp : PalaceBungee.getOnlinePlayers()) {
-                    tp.sendMessage(components);
+                    tp.sendMessage(message);
                 }
             }
         }, 1, 5, TimeUnit.MINUTES);
